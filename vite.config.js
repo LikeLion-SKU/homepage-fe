2bb237499123c0b94d3c654ea717dfa514c6adbf
Jeong mokjin <jungmokjin@gmail.com>
2026-01-18 21:27:31 +0900

    ✨ Feat: RootLayout에 Header 추가

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
