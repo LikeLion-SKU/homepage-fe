26dbd5396c0db033dbbc9621b70d987e69d97cc5
hyeonsu_ <117705439+agness4011@users.noreply.github.com>
2026-01-19 17:29:28 +0900

    ✨ Feat: 메인페이지 intro 수정 및 explain 섹션 추가

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,11 @@
+import Explain from '@/components/main/explain/Explain';
 import Intro from '@/components/main/intro/Intro';
 
 export default function Main() {
-  return <Intro />;
+  return (
+    <>
+      <Intro />
+      <Explain />
+    </>
+  );
 }
