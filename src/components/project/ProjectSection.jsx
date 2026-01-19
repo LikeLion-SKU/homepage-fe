1ca1757b45b804fc90ee84c7acac895bfb615f6d
Jeong mokjin <jungmokjin@gmail.com>
2026-01-19 13:37:30 +0900

    🎨 Project페이지 DummyData 추가

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -1,16 +1,12 @@
 import ProjectCard from '@/components/project/ProjectCard';
+import { projectData } from '@/components/project/projectDummyData';
 
 export default function ProjectSection() {
-  const data = {
-    imgUrl: '@/assets/icons/Logo_icon.png',
-    projectName: '단추',
-    ordinalNumber: 13,
-    contestName: '중앙톤',
-    explanation: '지류 쿠폰을 디지털화하여 편리하게 관리하고 단골이 되어보세요!',
-  };
   return (
     <div className="grid grid-cols-3 gap-x-7 gap-y-15 mt-15">
-      <ProjectCard props={data} />
+      {projectData.map((data) => (
+        <ProjectCard props={data} />
+      ))}
     </div>
   );
 }
