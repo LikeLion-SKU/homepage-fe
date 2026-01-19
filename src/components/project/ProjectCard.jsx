edf1ad1c01dd3c4c8f6c6f071f3fbefed0317fe9
Jeong mokjin <jungmokjin@gmail.com>
2026-01-19 15:52:11 +0900

    🎨 Design: 프로젝트 상세페이지 레이아웃 구현

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router';
 
 //@ts-ignore
 import Right from '@/assets/icons/right_icon.svg?react';
+import ProjectCategory from '@/components/project/ProjectCategory';
 
 export default function ProjectCard({ props }) {
   const navigate = useNavigate();
@@ -11,21 +12,15 @@ export default function ProjectCard({ props }) {
       className="group relative w-101 h-86 border overflow-hidden cursor-pointer"
     >
       <div className="flex flex-col w-101 h-86">
-        <img src={props.imgUrl} className="h-55 w-101" />
+        <img src={props.imgUrl} className="h-55 w-101 bg-[#D9D9D9]" />
         <div className="flex flex-col gap-3 w-101 h-29 p-5 ">
           <div className="flex justify-between items-center   ">
             <p className="text-[1.1rem] font-bold">{props.projectName}</p>
-            <div className="flex gap-1">
-              <div className="flex border rounded-3xl h-5 px-2 text-[0.9rem] text-center items-center">
-                수상작
-              </div>
-              <div className="flex border rounded-3xl h-5 px-2 text-[0.9rem] text-center items-center">
-                {props.ordinalNumber}기
-              </div>
-              <div className="flex border rounded-3xl h-5 px-2 text-[0.9rem] text-center items-center">
-                {props.contestName}
-              </div>
-            </div>
+            <ProjectCategory
+              isPrize={props.isPrize}
+              ordinalNumber={props.ordinalNumber}
+              contestName={props.contestName}
+            />
           </div>
           <p className="text-[0.9rem]">{props.explanation}</p>
         </div>
--- src/pages/Project/ProjectViewDetail.jsx
+++ src/pages/Project/ProjectViewDetail.jsx
@@ -1,3 +1,27 @@
+//@ts-ignore
+import Left from '@/assets/icons/left_anglebraket_icon.svg?react';
+//@ts-ignore
+import Right from '@/assets/icons/right_anglebraket_icon.svg?react';
+import ProjectDetailCard from '@/components/project/ProjectDetailCard';
+
 export default function ProjectViewDetail() {
-  return <></>;
+  const data = {
+    isPrize: 1,
+    imgUrl: '@/assets/icons/Logo_icon.png',
+    projectName: '단추',
+    ordinalNumber: 13,
+    contestName: '중앙톤',
+    explanation: '지류 쿠폰을 디지털화하여 편리하게 관리하고 단골이 되어보세요!',
+  };
+  return (
+    <div className="flex justify-center py-15 gap-7.5">
+      <div className="mt-85">
+        <Left />
+      </div>
+      <ProjectDetailCard props={data} />
+      <div className="mt-85">
+        <Right />
+      </div>
+    </div>
+  );
 }
