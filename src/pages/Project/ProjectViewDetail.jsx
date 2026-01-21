bef3b55f6170db95c1104bc5337d556eca26b8c8
Jeong mokjin <jungmokjin@gmail.com>
2026-01-21 19:02:36 +0900

    ✨ Feat: 토스트 컴포넌트 추가

--- src/pages/Project/ProjectViewDetail.jsx
+++ src/pages/Project/ProjectViewDetail.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Left from '@/assets/icons/left_anglebraket_icon.svg?react';
 //@ts-ignore
 import Right from '@/assets/icons/right_anglebraket_icon.svg?react';
+import Toast from '@/components/common/Toast/Toast';
 import ProjectDetailCard from '@/components/project/ProjectDetailCard';
 
 export default function ProjectViewDetail() {
@@ -46,19 +47,7 @@ export default function ProjectViewDetail() {
           다음 프로젝트 보기
         </p>
       </div>
-
-      <div
-        className={`fixed z-10 inset-0 flex items-center justify-center transition-all duration-500 ease-in-out
-        ${
-          isToast
-            ? 'opacity-100  pointer-events-auto' /*토스트 온이면 배경 화이트에 투명도 적용, 블러처리,마우스 이멘트 를 토스트가 받음*/
-            : 'opacity-0  pointer-events-none' /*부드럽게 가기위해 초기값을 다 0으로 설정,안보일 때는 마우스 이벤트 안 먹음 */
-        }`}
-      >
-        <div className="flex w-97 h-13 bg-white justify-center items-center text-[1.1rem] font-bold border shadow-lg">
-          마지막 프로젝트입니다.
-        </div>
-      </div>
+      <Toast isToast={isToast} />
     </div>
   );
 }
