5d33c2dfc912a92fc7dba72ed1362838fecb3ce6
Jeong mokjin <jungmokjin@gmail.com>
2026-01-22 23:04:20 +0900

    🎨 Design: GridSection 수정 및 추가

--- src/pages/Project/ProjectList.jsx
+++ src/pages/Project/ProjectList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import TitleSection from '@/components/common/TitleSection';
+import GridSection from '@/components/layout/background/GridSection';
 import ProjectOption from '@/components/project/ProjectOption';
 import ProjectPagenation from '@/components/project/ProjectPagenation';
 import ProjectSection from '@/components/project/ProjectSection';
@@ -30,20 +31,22 @@ export default function Project() {
   };
 
   return (
-    <div className=" flex flex-col px-14.5 py-12 ">
-      <TitleSection
-        title="프로젝트"
-        pageExplanation="서경대학교 멋쟁이사자처럼에서 탄생한 다양한 서비스를 둘러보세요!"
-      >
-        <ProjectOption />
-      </TitleSection>
-      <ProjectSection data={currentItems} />
-      <ProjectPagenation props={pageData} />
-      {!(projectData.length > 0) && (
-        <div className="flex w-330 h-40 justify-center items-center text-[1.1rem] font-bold">
-          검색 결과가 없습니다.
-        </div>
-      )}
-    </div>
+    <GridSection rows={21}>
+      <div className=" flex flex-col px-14.5 py-12 ">
+        <TitleSection
+          title="프로젝트"
+          pageExplanation="서경대학교 멋쟁이사자처럼에서 탄생한 다양한 서비스를 둘러보세요!"
+        >
+          <ProjectOption />
+        </TitleSection>
+        <ProjectSection data={currentItems} />
+        <ProjectPagenation props={pageData} />
+        {!(projectData.length > 0) && (
+          <div className="flex w-330 h-40 justify-center items-center text-[1.1rem] font-bold">
+            검색 결과가 없습니다.
+          </div>
+        )}
+      </div>
+    </GridSection>
   );
 }
