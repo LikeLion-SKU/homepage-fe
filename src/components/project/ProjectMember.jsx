87ba0dbf7d1df1f23383df0a49b22d3341c863b9
Jeong mokjin <jungmokjin@gmail.com>
2026-01-22 23:53:43 +0900

    🎨 Design: 프로젝트 상세 페이지 참여자 및 그리드 적용

--- src/components/project/ProjectMember.jsx
+++ src/components/project/ProjectMember.jsx
@@ -1,3 +1,16 @@
-export default function ProjectMember() {
-  return <div className="flex px-8 py-9 border w-84 h-58"></div>;
+export default function ProjectMember({ memberData }) {
+  return (
+    <div className="flex flex-col px-8 py-9 border w-84 h-58 gap-3">
+      {Object.entries(memberData).map(([role, names]) => (
+        <div key={role} className="flex w-68 text-[1rem] font-semibold">
+          <p>{role}</p>
+          <div className="flex gap-x-5 w-46 ml-auto flex-wrap">
+            {names.map((name, idx) => (
+              <p key={idx}>{name}</p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/pages/Project/ProjectViewDetail.jsx
+++ src/pages/Project/ProjectViewDetail.jsx
@@ -5,6 +5,7 @@ import Left from '@/assets/icons/left_anglebraket_icon.svg?react';
 //@ts-ignore
 import Right from '@/assets/icons/right_anglebraket_icon.svg?react';
 import Toast from '@/components/common/Toast/Toast';
+import GridSection from '@/components/layout/background/GridSection';
 import ProjectDetailCard from '@/components/project/ProjectDetailCard';
 
 export default function ProjectViewDetail() {
@@ -15,6 +16,12 @@ export default function ProjectViewDetail() {
     ordinalNumber: 13,
     contestName: '중앙톤',
     explanation: '지류 쿠폰을 디지털화하여 편리하게 관리하고 단골이 되어보세요!',
+    member: {
+      PM: ['신채린', '금시언'],
+      Design: ['김현수', '윤희준'],
+      Frontend: ['정영진', '임다현', '경찬희', '이은현'],
+      Backend: ['정목진', '심서현', '최운조'],
+    },
   };
   const [isToast, setIsToast] = useState(false);
   const onToastMessage = () => {
@@ -26,28 +33,30 @@ export default function ProjectViewDetail() {
   };
 
   return (
-    <div className="flex justify-center py-15 ">
-      <div className="flex flex-col ">
-        <div className="flex items-center justify-center rounded-l-2xl mt-85 w-20 h-32 bg-[#F9F9F9] ml-auto">
-          <Left />
+    <GridSection rows={23}>
+      <div className="flex justify-center pt-15 pb-63 ">
+        <div className="flex flex-col ">
+          <div className="flex items-center justify-center rounded-l-2xl mt-85 w-20 h-32 bg-[#F9F9F9] ml-auto">
+            <Left />
+          </div>
+          <p className="text-[0.7rem] text-[#B0B0B0] font-bold tracking-tighter mr-5">
+            이전 프로젝트 보기
+          </p>
         </div>
-        <p className="text-[0.7rem] text-[#B0B0B0] font-bold tracking-tighter mr-5">
-          이전 프로젝트 보기
-        </p>
+        <ProjectDetailCard data={data} />
+        <div className="flex flex-col ">
+          <button
+            onClick={() => onToastMessage()}
+            className="flex items-center justify-center rounded-r-2xl mt-85 w-20 h-32 bg-[#F9F9F9] mr-auto relative z-1"
+          >
+            <Right />
+          </button>
+          <p className="text-[0.7rem] text-[#B0B0B0] font-bold tracking-tighter ml-5">
+            다음 프로젝트 보기
+          </p>
+        </div>
+        <Toast isToast={isToast} message="마지막 프로젝트 입니다." />
       </div>
-      <ProjectDetailCard data={data} />
-      <div className="flex flex-col ">
-        <button
-          onClick={() => onToastMessage()}
-          className="flex items-center justify-center rounded-r-2xl mt-85 w-20 h-32 bg-[#F9F9F9] mr-auto relative z-1"
-        >
-          <Right />
-        </button>
-        <p className="text-[0.7rem] text-[#B0B0B0] font-bold tracking-tighter ml-5">
-          다음 프로젝트 보기
-        </p>
-      </div>
-      <Toast isToast={isToast} message="마지막 프로젝트 입니다." />
-    </div>
+    </GridSection>
   );
 }
