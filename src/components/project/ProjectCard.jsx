af9b4df55aa49bd43693474c3a44779d5472c890
Jeong mokjin <jungmokjin@gmail.com>
2026-01-23 11:08:39 +0900

    🎨 Design: 프젝 괸리 리스트 페이지 디자인 수정
    모달 추가 및 프로젝트섹션 패딩값 추가

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router';
 
 //@ts-ignore
 import Right from '@/assets/icons/right_navy_icon.svg?react';
+import AdminCardHover from '@/components/admin/project/ProjectCardHover';
 import ProjectCategory from '@/components/project/ProjectCategory';
 
 export default function ProjectCard({ props, isAdmin }) {
@@ -30,29 +31,7 @@ export default function ProjectCard({ props, isAdmin }) {
 
       <div className="absolute inset-0 bg-[rgba(137,137,137,0.62)] flex flex-col gap-2.5 items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
         {isAdmin ? (
-          <>
-            <div className="flex w-41 h-12 bg-[#F8F8F8]">
-              <div className="flex justify-center items-center bg-[#D9D9D9] w-12 h-12 border-r-[0.5px] border-[#8C8C8C]">
-                <Right />
-              </div>
-              <div
-                onClick={() => {
-                  navigate('/admin/project/edit');
-                }}
-                className="flex w-29 h-12 text-[1.1rem] justify-center items-center"
-              >
-                수정하기
-              </div>
-            </div>
-            <div className="flex w-41 h-12 bg-[#F8F8F8]">
-              <div className="flex justify-center items-center bg-[#D9D9D9] w-12 h-12 border-r-[0.5px] border-[#8C8C8C]">
-                <Right />
-              </div>
-              <div className="flex w-29 h-12 text-[1.1rem] justify-center items-center">
-                삭제하기
-              </div>
-            </div>
-          </>
+          <AdminCardHover />
         ) : (
           <div className="flex w-41 h-12 bg-[#F8F8F8] border">
             <div className="flex justify-center items-center bg-[#C6E400] w-12 border-r ">
