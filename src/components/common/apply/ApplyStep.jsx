780c950e4626a355a4bd037cc04f4e6ed5e1a208
simGPT <simGPT02@gmail.com>
2026-01-24 00:28:15 +0900

    :art:Design: 좌우 배치 변경

--- src/components/common/apply/ApplyStep.jsx
+++ src/components/common/apply/ApplyStep.jsx
@@ -1,12 +1,10 @@
 export default function ApplyStep({ step, stepName, lineStyle, stepStyle, stepNameStyle }) {
   return (
-    <div>
-      <div>
-        <div className="flex flex-col">
-          <div className={lineStyle} />
-          <div className={stepStyle}>{step}</div>
-          <div className={stepNameStyle}>{stepName}</div>
-        </div>
+    <div className="flex-1">
+      <div className="flex flex-col">
+        <div className={lineStyle} />
+        <div className={stepStyle}>{step}</div>
+        <div className={stepNameStyle}>{stepName}</div>
       </div>
     </div>
   );
