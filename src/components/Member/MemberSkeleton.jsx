4b417595df9034f10262e56cedd682ccd5bacb7b
Jeong mokjin <jungmokjin@gmail.com>
2026-01-25 21:49:57 +0900

    🎨 Design: 구성원 페이지 스켈레톤 추가

---
+++ src/components/Member/MemberSkeleton.jsx
@@ -0,0 +1,7 @@
+export default function MemberSkeleton() {
+  return (
+    <div className=" h-77 border animate-pulse bg-[#D9D9D9]">
+      <div className="w-59 h-54 bg-[#B0B0B0]"></div>
+    </div>
+  );
+}
--- src/pages/Member/Member.jsx
+++ src/pages/Member/Member.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
+
 import BackgroundImg1 from '@/assets/images/member_background1.svg';
 import BackgroundImg2 from '@/assets/images/member_background2.svg';
 import BackgroundImg3 from '@/assets/images/member_background3.svg';
-import MemberSection from '@/components/Member/memberSection';
+import MemberOption from '@/components/Member/MemberOption';
+import MemberSection from '@/components/Member/MemberSection';
 import TitleSection from '@/components/common/TitleSection';
-import MemberOption from '@/pages/Member/MemberOption';
 import { memberData1, memberData2 } from '@/pages/Member/memberDummyData';
 
 export default function Member() {
   const optionData = ['14기', '13기', '12기', '11기'];
+  const [selectedOption, setSelectedOption] = useState('14기');
   return (
     <div className="flex flex-col p-14 relative mb-70">
       <TitleSection
@@ -15,7 +18,11 @@ export default function Member() {
         pageExplanation="서경대학교 멋쟁이사자처럼의 구성원들을 살펴보세요."
         onSearch={false}
       >
-        <MemberOption optionData={optionData} />
+        <MemberOption
+          optionData={optionData}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+        />
       </TitleSection>
       <div className="flex flex-col gap-32 pl-7 pt-20">
         <MemberSection title="운영진" data={memberData1} />
