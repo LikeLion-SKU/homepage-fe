8e8e9dcc9c743a9261e4963301c4897d1cca8265
Jeong mokjin <jungmokjin@gmail.com>
2026-01-25 22:02:59 +0900

    🎨 Design: 프로젝트 페이지 스켈레톤 추가

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -1,27 +1,44 @@
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 
 //@ts-ignore
 import Plus from '@/assets/icons/plus_icon.svg?react';
 import ProjectCard from '@/components/project/ProjectCard';
+import ProjectSkeleton from '@/components/project/ProjectSkeleton';
 
 export default function ProjectSection({ data }) {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const isAdmin = pathname.startsWith('/admin');
+  const [isLoading, setIsLoading] = useState(true);
+  const handleLoading = () => {
+    setIsLoading(true);
+  }; //esLint 제거를 위해 임시로 넣음
+  handleLoading;
 
   return (
-    <div className="flex flex-col mt-6 px-7">
-      {isAdmin && (
-        <div
-          onClick={() => navigate('/admin/project/edit')}
-          className="flex h-15 border justify-center items-center "
-        >
-          <Plus />
+    <>
+      {isLoading ? (
+        <div className="grid grid-cols-3 gap-x-7 gap-y-15 mt-12 px-7">
+          {Array.from({ length: 6 }).map(() => (
+            <ProjectSkeleton />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col mt-6 px-7">
+          {isAdmin && (
+            <div
+              onClick={() => navigate('/admin/project/edit')}
+              className="flex h-15 border justify-center items-center "
+            >
+              <Plus />
+            </div>
+          )}
+          <div className="grid grid-cols-3 gap-x-7 gap-y-15 mt-6">
+            {data.length > 0 && data.map((data) => <ProjectCard props={data} isAdmin={isAdmin} />)}
+          </div>
         </div>
       )}
-      <div className="grid grid-cols-3 gap-x-7 gap-y-15 mt-6">
-        {data.length > 0 && data.map((data) => <ProjectCard props={data} isAdmin={isAdmin} />)}
-      </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/project/ProjectSkeleton.jsx
@@ -0,0 +1,7 @@
+export default function ProjectSkeleton() {
+  return (
+    <div className="w-101 h-86 border animate-pulse bg-white">
+      <div className="h-57 bg-[#B0B0B0]"></div>
+    </div>
+  );
+}
