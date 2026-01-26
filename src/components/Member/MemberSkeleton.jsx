a7701795759c28b5dd83e979428b80ae9b300e6b
Jeong mokjin <jungmokjin@gmail.com>
2026-01-26 15:34:18 +0900

    🎨 Design: 스켈레톤 텍스트 추가

--- src/components/Member/MemberSkeleton.jsx
+++ src/components/Member/MemberSkeleton.jsx
@@ -1,7 +1,11 @@
 export default function MemberSkeleton() {
   return (
-    <div className=" h-77 border animate-pulse bg-[#D9D9D9]">
-      <div className="w-59 h-54 bg-[#B0B0B0]"></div>
+    <div className="w-59 h-80 border animate-pulse bg-white">
+      <div className="h-54 bg-[#B0B0B0]"></div>
+      <div className="flex flex-col px-6 py-5.5 gap-4">
+        <div className="w-15 h-5 rounded-2xl bg-[#B0B0B0]" />
+        <div className="w-30 h-5 rounded-2xl bg-[#B0B0B0]" />
+      </div>
     </div>
   );
 }
