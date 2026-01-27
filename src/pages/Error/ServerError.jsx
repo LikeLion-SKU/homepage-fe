2eec0849f57f8c952f1e930973aa92c3b943a7b8
simGPT <simGPT02@gmail.com>
2026-01-27 15:24:59 +0900

    :sparkles:Feat: 서버 오류/로그인 필요 페이지 UI

---
+++ src/pages/Error/ServerError.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router';
+
+import GridSection from '@/components/background/GridSection';
+import RedirectButton from '@/components/common/Button/RedirectButton';
+import StatusMessage from '@/components/common/StatusMessage';
+
+export default function ServerError() {
+  const title = '요청하신 작업을 수행하지 못했습니다.';
+  const content = '서버에 오류가 발생했습니다.\n이용에 불편을 드려 죄송합니다.';
+  const buttonName = '홈으로 돌아가기';
+  const navigate = useNavigate();
+  return (
+    <GridSection>
+      <div className="flex flex-col min-h-screen justify-center items-center gap-16">
+        <StatusMessage title={title} content={content}></StatusMessage>
+        <RedirectButton buttonName={buttonName} onClick={navigate('/')}></RedirectButton>
+      </div>
+    </GridSection>
+  );
+}
