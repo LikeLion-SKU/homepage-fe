1a095f48f5f994bad9576bf9abb02412ae122c49
simGPT <simGPT02@gmail.com>
2026-01-30 18:57:36 +0900

    :art:Design: 지원서 작성 공통질문 페이지 모바일, 패드, 웹 버전 반응형 추가

--- src/pages/Apply/ApplyCommon.jsx
+++ src/pages/Apply/ApplyCommon.jsx
@@ -44,41 +44,41 @@ export default function ApplyCommon() {
 
   return (
     <div className="pb-35">
-      <div className="w-full flex flex-col pt-18 pad:px-15 web:px-45.5 gap-23">
-        <div className="flex flex-col gap-31">
-          <div className="flex flex-col gap-20">
+      <div className="w-full flex flex-col pt-18 px-6 pad:px-15 web:px-45.5 gap-23">
+        <div className="flex flex-col gap-20 pad:gap-31">
+          <div className="flex flex-col gap-15 pad:gap-20">
             {/* 지원서 작성 페이지의 기본 정보 섹션 */}
             <ApplyTitleSection></ApplyTitleSection>
             {/* 지원서 작성 단계 부분 */}
-            <div className="self-stretch inline-flex items-center gap-7">
+            <div className="self-stretch inline-flex items-start gap-7">
               <ApplyStep
                 step="STEP 1"
                 stepName="기본 인적사항"
                 lineStyle="self-stretch h-1 bg-navy-blue mb-5"
-                stepStyle="self-stretch text-center text-navy-blue text-1xl font-medium font-['Pretendard'] mb-1.5"
-                stepNameStyle="self-stretch text-center text-navy-blue text-2xl font-bold font-['Pretendard']"
+                stepStyle="self-stretch text-center text-navy-blue text-sm font-semibold pad:text-1xl pad:font-medium mb-1.5"
+                stepNameStyle="self-stretch text-center text-navy-blue text-lg pad:text-2xl font-bold"
               ></ApplyStep>
               <ApplyStep
                 step="STEP 2"
                 stepName="공통 질문"
                 lineStyle="self-stretch h-1 bg-button-green mb-5"
-                stepStyle="self-stretch text-center text-button-green text-1xl font-medium font-['Pretendard'] mb-1.5"
-                stepNameStyle="self-stretch text-center text-button-green text-2xl font-bold font-['Pretendard']"
+                stepStyle="self-stretch text-center text-button-green text-sm font-semibold pad:text-1xl pad:font-medium mb-1.5"
+                stepNameStyle="self-stretch text-center text-button-green text-lg pad:text-2xl font-bold"
               ></ApplyStep>
               <ApplyStep
                 step="STEP 3"
                 stepName="트랙별 질문"
                 lineStyle="self-stretch h-1 bg-navy-blue mb-5"
-                stepStyle="self-stretch text-center text-navy-blue text-1xl font-medium font-['Pretendard'] mb-1.5"
-                stepNameStyle="self-stretch text-center text-navy-blue text-2xl font-bold font-['Pretendard']"
+                stepStyle="self-stretch text-center text-navy-blue text-sm font-semibold pad:text-1xl pad:font-medium mb-1.5"
+                stepNameStyle="self-stretch text-center text-navy-blue text-lg pad:text-2xl font-bold"
               ></ApplyStep>
             </div>
           </div>
           {/* 공통질문 부분 */}
-          <div className="flex flex-col gap-10">
-            <div className="self-stretch h-8 text-2xl font-bold font-['Pretendard']">공통 질문</div>
+          <div className="flex flex-col gap-4 pad:gap-10">
+            <div className="self-stretch h-8 text-lg pad:text-2xl font-bold">공통 질문</div>
             {/* 공통질문 상자 */}
-            <div className="flex flex-col pad:px-10 web:px-20 pad:py-18 web:py-18.5 border bg-button-gray gap-15">
+            <div className="flex flex-col px-6 py-7 pad:px-10 web:px-20 pad:py-18 web:py-18.5 border bg-button-gray gap-15">
               {/* 공통질문 내용 */}
               {QUESTION_LIST.filter((item) => item.track === 'COMMON').map(
                 (
@@ -102,23 +102,29 @@ export default function ApplyCommon() {
             <div className="flex flex-col pad:flex-row justify-center items-center gap-5">
               <Button
                 onClick={() => {}}
-                className="w-45 pad:w-47 web:w-53 h-14 outline -outline-offset-1 outline-text-gray flex justify-center items-center bg-white transition-all hover:bg-stone-100"
+                className="w-45 pad:w-47 web:w-53 h-11 pad:h-14 outline -outline-offset-1 outline-text-gray flex justify-center items-center bg-white transition-all hover:bg-stone-100"
               >
-                <span className=" text-text-gray text-lg font-medium">임시저장</span>
+                <span className=" text-gray-900 text-base font-semibold pad:text-lg pad:font-medium">
+                  임시저장
+                </span>
               </Button>
 
               <Button
                 onClick={handlePrevious}
-                className="w-45 pad:w-47 web:w-53 h-14 outline -outline-offset-1 outline-text-gray flex justify-center items-center bg-white transition-all hover:bg-stone-100"
+                className="w-45 pad:w-47 web:w-53 h-11 pad:h-14 outline -outline-offset-1 outline-text-gray flex justify-center items-center bg-white transition-all hover:bg-stone-100"
               >
-                <span className=" text-text-gray text-lg font-medium">이전단계</span>
+                <span className=" text-gray-900 text-base font-semibold pad:text-lg pad:font-medium">
+                  이전단계
+                </span>
               </Button>
 
               <Button
                 onClick={handleNext} // 추후 다음 페이지 추가 필요
-                className="w-45 pad:w-47 web:w-53 h-14 bg-button-green outline -outline-offset-1 outline-black flex justify-center items-center transition-all hover:bg-button-hover"
+                className="w-45 pad:w-47 web:w-53 h-11 pad:h-14 bg-button-green outline -outline-offset-1 outline-black flex justify-center items-center transition-all hover:bg-button-hover"
               >
-                <span className="opacity-70 text-black text-lg font-medium">다음단계</span>
+                <span className="opacity-70 text-black text-base font-semibold pad:text-lg pad:font-medium">
+                  다음단계
+                </span>
               </Button>
             </div>
           </div>
