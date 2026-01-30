be35d8a6bcc7ced2efeb451a77de7c45c5fbb85f
simGPT <simGPT02@gmail.com>
2026-01-30 14:11:37 +0900

    :sparkles:Feat: 반응형 임시 커밋

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -51,112 +51,114 @@ export default function MyPage() {
   };
 
   return (
-    <div className="w-full flex flex-col web:flex-row web:justify-between min-h-screen relative bg-white">
-      {/* 왼쪽 부분 */}
-      <div className="relative px-46.25 flex justify-start items-center gap-9">
-        <div className="w-40 h-40 pad:w-44 pad:h-44 relative">
-          <div className="relative w-44 h-44 pad:w-44 pad:h-44 bg-zinc-300 border border-black group">
-            {/* 이 label을 누르면 input으로 입력받을 수 있게 */}
-            <label
-              htmlFor="profile-upload"
-              className="cursor-pointer w-full h-full flex items-center justify-center"
-            >
-              <img
-                src={
-                  isError
-                    ? defaultProfileImage
-                    : preview || userData.profileImage || defaultProfileImage
-                }
-                onError={() => setIsError(true)}
-                className="w-full h-full object-cover group-hover:brightness-75"
-                alt="프로필"
-              ></img>{' '}
-              {/* 프로필 사진 */}
-              <div className="w-8 h-8 absolute overflow-hidden">
-                <img src={Camera}></img> {/* 카메라 아이콘 */}
-              </div>
-            </label>
-            {/* 실제 프로필 사진 입력받는 부분 -> 가림 */}
-            <input
-              id="profile-upload"
-              type="file"
-              accept="image/*"
-              className="hidden"
-              onChange={handleFileChange} // 파일 선택 시 실행될 함수
-            />
+    <div className="w-full min-h-screen relative bg-white">
+      <div className="flex flex-col web:flex-row web:items-start items-center web:justify-between px-6 pad:px-20 web:px-46.25">
+        {/* 왼쪽 부분 */}
+        <div className="relative flex flex-col web:flex-row justify-start items-center gap-9">
+          <div className="w-40 h-40 pad:w-44 pad:h-44 relative">
+            <div className="relative w-44 h-44 pad:w-44 pad:h-44 bg-zinc-300 border border-black group">
+              {/* 이 label을 누르면 input으로 입력받을 수 있게 */}
+              <label
+                htmlFor="profile-upload"
+                className="cursor-pointer w-full h-full flex items-center justify-center"
+              >
+                <img
+                  src={
+                    isError
+                      ? defaultProfileImage
+                      : preview || userData.profileImage || defaultProfileImage
+                  }
+                  onError={() => setIsError(true)}
+                  className="w-full h-full object-cover group-hover:brightness-75"
+                  alt="프로필"
+                ></img>{' '}
+                {/* 프로필 사진 */}
+                <div className="w-8 h-8 absolute overflow-hidden">
+                  <img src={Camera}></img> {/* 카메라 아이콘 */}
+                </div>
+              </label>
+              {/* 실제 프로필 사진 입력받는 부분 -> 가림 */}
+              <input
+                id="profile-upload"
+                type="file"
+                accept="image/*"
+                className="hidden"
+                onChange={handleFileChange} // 파일 선택 시 실행될 함수
+              />
+            </div>
+          </div>
+          <div className="min-w-64 pad:min-w-72 web:max-w-80 flex flex-col items-center web:items-start text-center web:text-left gap-3">
+            <div className="self-stretch justify-center">
+              <span className="text-black text-xl pad:text-2xl font-bold font-['Pretendard']">
+                {userData.name}
+              </span>
+              <span className="text-black text-4xl font-bold font-['Pretendard']"> </span>
+              <span className="text-zinc-600 text-lg pad:text-xl  font-semibold font-['Pretendard']">
+                님, 안녕하세요
+              </span>
+            </div>
+            <div className="self-stretch justify-start text-stone-500 text-sm pad:text-lg font-semibold pad:font-medium font-['Pretendard']">
+              {userData.email}
+            </div>
           </div>
         </div>
-        <div className="min-w-64 pad:min-w-72 web:min-w-80 inline-flex flex-col justify-start items-start gap-3">
-          <div className="self-stretch justify-center">
-            <span className="text-black text-xl pad:text-2xl font-bold font-['Pretendard']">
-              {userData.name}
-            </span>
-            <span className="text-black text-4xl font-bold font-['Pretendard']"> </span>
-            <span className="text-zinc-600 text-lg pad:text-xl  font-semibold font-['Pretendard']">
-              님, 안녕하세요
-            </span>
-          </div>
-          <div className="self-stretch justify-start text-stone-500 text-sm pad:text-lg font-semibold pad:font-medium font-['Pretendard']">
-            {userData.email}
-          </div>
-        </div>
-      </div>
-      {/* 오른쪽 부분 */}
-      <div className="w-60 pad:max-w-142 pad:min-w-48 web:min-w-96 left-222.5 top-55.25 absolute inline-flex flex-col justify-start items-start gap-4">
-        <div className="self-stretch">
-          <Button
-            onClick={() => {
-              if (hasApplication) {
-                navigate('/application');
-              } else {
-                navigate('/apply');
-              }
-            }}
-            data-variant=""
-            data-size=""
-            className={buttonStyle}
-          >
-            {hasApplication ? '내 지원서 보러가기' : '지원서 작성하기'}
-          </Button>
-        </div>
-        {/* 면접 예약 여부로 생겼다가 없어져야 하는 면접 일정 수정하기 버튼 -> 추후 어색하면 스켈레톤 넣자 */}
-        {isInterviewReserved && (
+        {/* 오른쪽 부분 */}
+        <div className="w-full pad:max-w-142 pad:min-w-48 web:min-w-96 flex flex-col justify-start items-start gap-4">
           <div className="self-stretch">
             <Button
               onClick={() => {
-                navigate('/result');
+                if (hasApplication) {
+                  navigate('/application');
+                } else {
+                  navigate('/apply');
+                }
               }}
               data-variant=""
               data-size=""
               className={buttonStyle}
             >
-              면접 일정 수정하기
+              {hasApplication ? '내 지원서 보러가기' : '지원서 작성하기'}
             </Button>
           </div>
-        )}
-        <div className="self-stretch">
-          <Button
-            onClick={() => {
-              navigate('/mypage/password/change');
-            }}
-            data-variant=""
-            data-size=""
-            className={buttonStyle}
-          >
-            비밀번호 변경
-          </Button>
-        </div>
-        <div className="self-stretch">
-          <Button
-            onClick={() => {
-              setIsModalOpen(true);
-            }}
-            data-variant=""
-            data-size=""
-            className={buttonStyle}
-          >
-            로그아웃
-          </Button>
+          {/* 면접 예약 여부로 생겼다가 없어져야 하는 면접 일정 수정하기 버튼 -> 추후 어색하면 스켈레톤 넣자 */}
+          {isInterviewReserved && (
+            <div className="self-stretch">
+              <Button
+                onClick={() => {
+                  navigate('/result');
+                }}
+                data-variant=""
+                data-size=""
+                className={buttonStyle}
+              >
+                면접 일정 수정하기
+              </Button>
+            </div>
+          )}
+          <div className="self-stretch">
+            <Button
+              onClick={() => {
+                navigate('/mypage/password/change');
+              }}
+              data-variant=""
+              data-size=""
+              className={buttonStyle}
+            >
+              비밀번호 변경
+            </Button>
+          </div>
+          <div className="self-stretch">
+            <Button
+              onClick={() => {
+                setIsModalOpen(true);
+              }}
+              data-variant=""
+              data-size=""
+              className={buttonStyle}
+            >
+              로그아웃
+            </Button>
+          </div>
         </div>
       </div>
       <Modal
