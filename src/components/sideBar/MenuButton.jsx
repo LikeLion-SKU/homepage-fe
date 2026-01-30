4e6bc30b96c733a9d4ff97bc4eae22d963302961
Jeong mokjin <jungmokjin@gmail.com>
2026-01-30 18:13:15 +0900

    🎨 Design: 타이틀 섹션 반응형 추가

--- src/components/sideBar/MenuButton.jsx
+++ src/components/sideBar/MenuButton.jsx
@@ -12,9 +12,9 @@ export default function MenuButton({ name, path, handleSideBar }) {
         handleSideBar();
       }}
       className="w-full flex h-15 justify-between items-center bg-[#F9F9F9] pl-7 pr-9 
-        pad:pl-17 pad:pr-21 pad:text-[1.4rem] pad:font-semibold"
+        pad:pl-17 pad:pr-21 pad:font-semibold"
     >
-      <p className="text-[1rem]">{name}</p>
+      <p className="text-[1rem] pad:text-[1.4rem]">{name}</p>
       <RightDown />
     </button>
   );
