1476b9947ffe5dbdd3407946130bb73c63d66682
simGPT <simGPT02@gmail.com>
2026-01-31 18:18:41 +0900

    :art:Design: 에러페이지 및 지원완료 페이지 반응형 구현

--- src/components/common/Button/RedirectButton.jsx
+++ src/components/common/Button/RedirectButton.jsx
@@ -3,17 +3,17 @@ import Button from '@/components/common/Button/Button';
 
 export default function RedirectButton({ buttonName, onClick }) {
   return (
-    <div className="flex w-54 justify-start items-center z-10">
+    <div className="flex justify-center items-center z-10">
       <Button
         onClick={onClick}
         data-variant=""
         data-size=""
-        className="w-12 h-12 bg-button-green hover:bg-button-hover px-3 py-3.5 outline flex-col justify-center items-center gap-2.5"
+        className="w-10 h-10 pad:w-12 pad:h-12 bg-button-green hover:bg-button-hover outline flex flex-col justify-center items-center gap-2.5"
       >
-        <img src={Navy} className="w-6 h-5" alt="navy icon" />
+        <img src={Navy} className="w-4 h-4 pad:w-6 pad:h-5" alt="navy icon" />
       </Button>
-      <div className="self-stretch h-12 px-4 py-3 outline bg-button-gray relative z-10">
-        <div className="justify-center text-zinc-900 text-lg font-medium font-['Pretendard']">
+      <div className="self-stretch h-10 pad:h-12 px-4 py-3 outline bg-button-gray relative z-10 flex items-center justify-center">
+        <div className="justify-center text-zinc-900 text-sm font-semibold pad:text-lg pad:font-medium font-['Pretendard']">
           {buttonName}
         </div>
       </div>
