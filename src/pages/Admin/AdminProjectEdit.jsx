3f2ce218582a0ba76b0ae87b68132c2f9aa5ce6f
Jeong mokjin <jungmokjin@gmail.com>
2026-02-01 16:18:34 +0900

    ✨ Feat: 유효성 검사 추가, 더미데이터 연결

--- src/pages/Admin/AdminProjectEdit.jsx
+++ src/pages/Admin/AdminProjectEdit.jsx
@@ -17,25 +17,42 @@ export default function AdminProjectEdit() {
     ordinalNum: '',
     prize: false,
     contest: '',
+    selectedTrack: {},
   });
+  const semesterOption = ['14기', '13기', '12기', '11기'];
+  const prizeOption = ['수상O', '수상X'];
+  const contestOption = ['중앙해커톤', '아이디어톤', '4호선톤', '교내해커톤'];
+  const trackOption = ['PO', 'PM', 'Design', 'Frontend', 'Backend'];
+  const [imgNum, setImgNum] = useState(0);
+  const imgCount = data.imgUrl.length;
   const navigate = useNavigate();
   //@ts-ignore
   const { showToast } = useOutletContext();
   const fileInputRef = useRef(null);
+
+  const handleSelectedTrack = (callback) => {
+    setData((prev) => ({
+      ...prev,
+      selectedTrack: callback(prev.selectedTrack),
+    }));
+  };
+  const handleExplainChange = (e) => {
+    const value = e.target.value;
+    if (value.length <= 300) {
+      // 300자 이하일 때만 상태 업데이트
+      setData((prev) => ({ ...prev, explain: value }));
+    }
+  };
   const imgFilePlus = (e) => {
     const files = Array.from(e.target.files);
-
     if (files.length > 0) {
       // 2. 선택된 파일들을 순회하며 미리보기 URL 생성
       const newPreviews = files.map((file) => URL.createObjectURL(file));
-
       // 기존 이미지에 새로 추가된 이미지를 합칩니다.
       setData((prev) => ({
         ...prev, // 1. 기존의 projectName, explain 등 다른 값들은 그대로 유지
         imgUrl: [...prev.imgUrl, ...newPreviews], // 2. imgUrl 배열만 기존 값 + 새 값으로 합치기
       }));
-
-      console.log('선택된 파일들:', files);
     }
   };
   const imgFileDelete = () => {
@@ -53,11 +70,6 @@ export default function AdminProjectEdit() {
       setImgNum(nextImgUrl.length - 1);
     }
   };
-  const optionData = ['14기', '13기', '12기', '11기'];
-  const [imgNum, setImgNum] = useState(0);
-  const imgCount = data.imgUrl.length;
-  const [selectedTrack, setSelectedTrack] = useState({});
-
   const changeImg = async (direction) => {
     const nextNum = imgNum + direction;
     // 이미지 배열의 길이를 확인하여 처음과 끝 처리
@@ -67,6 +79,25 @@ export default function AdminProjectEdit() {
       setImgNum(0);
     } else {
       setImgNum(nextNum);
+    }
+  };
+
+  const saveProject = () => {
+    if (data.imgUrl.length < 1) {
+      showToast('이미지를 1개 이상 첨부해주세요.');
+    } else if (!data.projectName) {
+      showToast('프로젝트명을 입력해주세요.');
+    } else if (!data.contest) {
+      showToast('대회명을 입력해주세요.');
+    } else if (!data.ordinalNum) {
+      showToast('기수를 입력해주세요.');
+    } else if (!data.explain) {
+      showToast('설명을 입력해주세요.');
+    } else if (!data.selectedTrack) {
+      showToast('멤버를 입력해주세요.');
+    } else {
+      navigate('/admin/project');
+      showToast('저장되었습니다.');
     }
   };
 
@@ -85,30 +116,37 @@ export default function AdminProjectEdit() {
         <div className="flex justify-between">
           <div className="flex flex-col gap-8 w-164">
             <input
+              value={data.projectName}
+              onChange={(e) => setData((prev) => ({ ...prev, projectName: e.target.value }))}
               placeholder={data.projectName ? data.projectName : '프로젝트 이름'}
               className="text-[2rem] font-bold placeholder:text-black focus:outline-none"
             />
             <div className="flex gap-2.5">
               <OptionBox
                 initValue={data.ordinalNum ? data.ordinalNum : '기수선택'}
-                optionData={optionData}
+                optionData={semesterOption}
                 bgColor="#D9D9D9"
               />
               <OptionBox
-                initValue={data.ordinalNum ? data.ordinalNum : '기수선택'}
-                optionData={optionData}
+                initValue={data.prize ? data.prize : '수상여부'}
+                optionData={prizeOption}
                 bgColor="#D9D9D9"
               />
               <OptionBox
-                initValue={data.ordinalNum ? data.ordinalNum : '기수선택'}
-                optionData={optionData}
+                initValue={data.contest ? data.contest : '대회선택'}
+                optionData={contestOption}
                 bgColor="#D9D9D9"
               />
             </div>
-            <textarea
-              placeholder={data.explain ? data.explain : '프로젝트 설명'}
-              className="h-42 text-[1.1rem] w-164 placeholder:text-black "
-            />
+            <div>
+              <textarea
+                value={data.explain}
+                onChange={(e) => handleExplainChange(e)}
+                placeholder={data.explain ? data.explain : '프로젝트 설명'}
+                className="h-42 text-[1.1rem] w-164 placeholder:text-black "
+              />
+              <div className="text-right text-gray-400 text-sm">{data.explain.length} / 300</div>
+            </div>
           </div>
 
           <div className="flex flex-col gap-11 ">
@@ -143,19 +181,16 @@ export default function AdminProjectEdit() {
               </div>
             </div>
             <AdminProjectMember
-              optionData={optionData}
-              selectedTrack={selectedTrack}
-              setSelectedTrack={setSelectedTrack}
+              optionData={trackOption}
+              selectedTrack={data.selectedTrack}
+              setSelectedTrack={handleSelectedTrack}
             />
           </div>
         </div>
       </div>
       <div className="flex w-285 pl-120">
         <button
-          onClick={() => {
-            navigate('/admin/project');
-            showToast('저장되었습니다.');
-          }}
+          onClick={() => saveProject()}
           className="w-40 h-12 border bg-[#D9D9D9] text-center items-center"
         >
           저장하기
