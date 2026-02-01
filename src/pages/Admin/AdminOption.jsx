2b4b1a99fe90ade376a12fdea250ad59c25cbbb0
Jeong mokjin <jungmokjin@gmail.com>
2026-02-01 15:00:31 +0900

    ✨ Feat: 저장시 모달 및 토스트  추가

--- src/pages/Admin/AdminOption.jsx
+++ src/pages/Admin/AdminOption.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useOutletContext } from 'react-router';
 
 import OptionAdminTable from '@/components/admin/Option/OptionAdminTable';
 import OptionTitle from '@/components/admin/Option/OptionTitle';
@@ -12,6 +13,12 @@ export default function AdminOption() {
     '트렌디톤',
     'SKTHON',
   ]);
+  //@ts-ignore
+  const { openModal, showToast } = useOutletContext();
+  const saveOption = () => {
+    //저장 api 추가 예정
+    showToast('저장되었습니다!');
+  };
   return (
     <div className="flex flex-col gap-20 px-21 py-30">
       <OptionTitle />
@@ -19,7 +26,10 @@ export default function AdminOption() {
         <OptionAdminTable title="기수" optionData={ordinalData} setOptionData={setOrdinalData} />
         <OptionAdminTable title="대회명" optionData={contestData} setOptionData={setContestData} />
       </div>
-      <button className="w-43 h-12 border text-center items-center bg-[#D9D9D9] mx-auto">
+      <button
+        onClick={() => openModal('저장하시겠습니까?', () => saveOption())}
+        className="w-43 h-12 border text-center items-center bg-[#D9D9D9] mx-auto"
+      >
         저장하기
       </button>
     </div>
