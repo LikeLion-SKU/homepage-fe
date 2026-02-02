fd723b3922030e59705a866c6649baf9c3089188
Jeong mokjin <jungmokjin@gmail.com>
2026-02-02 20:38:40 +0900

    ✨ Feat: api 연결 준비

---
+++ src/api/projectApi.js
@@ -0,0 +1,8 @@
+import { APIService } from '@/api/api';
+
+export const getProjectList = async () => {
+  // try{
+  //     const res=await APIService.public.get()
+  // } catch(error){
+  // }
+};
---
+++ src/api/userApi.js
@@ -0,0 +1,7 @@
+import { APIService } from '@/api/api';
+
+export const getClubMember = () => {
+  // try{
+  //     const res=APIService.public.get()
+  // }
+};
