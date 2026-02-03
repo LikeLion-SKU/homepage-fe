11162685709617b1261f1cc69174b288fc85c1b5
Jeong mokjin <jungmokjin@gmail.com>
2026-02-03 15:47:59 +0900

    ✨ Feat: 기수 조회 연결

--- src/api/semesterApi.js
+++ src/api/semesterApi.js
@@ -4,7 +4,7 @@ export const getSemester = async () => {
   try {
     const res = await APIService.public.get('/v1/semesters');
 
-    return res.data;
+    return res.data.map((item) => `${item.semester}기`);
   } catch (error) {
     console.log('기수 목록 조회 실패:', error);
   }
--- src/components/project/ProjectOption.jsx
+++ src/components/project/ProjectOption.jsx
@@ -1,30 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import { getProjectType } from '@/api/projectApi';
+import { getSemester } from '@/api/semesterApi';
 import OptionBox from '@/components/common/Option/optionBox';
 import { useIsPhone } from '@/hooks/useIsPhone';
 
 export default function ProjectOption() {
-  const ordirnalNum = ['전체', '14기', '13기', '12기', '11기'];
-  const contestName = [
-    '전체',
-    '중앙해커톤',
-    '아이디어톤',
-    '4호선톤',
-    '트렌디톤',
-    '블라블라',
-    '블블라',
-  ];
+  const [semesters, setSemesters] = useState([]);
+  const [projectTypes, setProjectTypes] = useState([]);
   const [selectMenu, setSelectMenu] = useState('전체');
   const isPhone = useIsPhone();
+  useEffect(() => {
+    const getOptionData = async () => {
+      try {
+        setSemesters(await getSemester());
+        setProjectTypes(await getProjectType());
+      } catch (error) {
+        console.log('옵션 데이터 조회 실패:', error);
+      }
+    };
+    getOptionData();
+  }, []);
 
   return (
     <div className="flex  h-20 items-center gap-2 pad:gap-15">
-      <OptionBox initValue="기수별" optionData={ordirnalNum} bgColor="#E9E9E9" />
+      <OptionBox initValue="기수별" optionData={semesters} bgColor="#E9E9E9" />
       {isPhone ? (
-        <OptionBox initValue="대회별" optionData={contestName} bgColor="#E9E9E9" />
+        <OptionBox initValue="대회별" optionData={projectTypes} bgColor="#E9E9E9" />
       ) : (
         <div className="flex flex-1 min-w-0 h-10 text-[1rem] pad:gap-5 items-center overflow-x-auto no-scrollbar border-r">
-          {contestName.map((name) => (
+          {projectTypes.map((name) => (
             <div
               key={name}
               onClick={() => setSelectMenu(name)}
