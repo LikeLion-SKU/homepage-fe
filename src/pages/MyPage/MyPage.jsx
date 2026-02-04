2233e9a4aba3a56765d560c769ab40924e98f5ce
simGPT <simGPT02@gmail.com>
2026-02-04 14:33:13 +0900

    :sparkles:Feat: 마이페이지 정보 조회 api 연동

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
+import { useFetcher } from 'react-router-dom';
 
 import Home from '@/assets/icons/4.svg';
 import Camera from '@/assets/icons/mdi-light_camera.svg';
@@ -8,20 +9,27 @@ import Button from '@/components/common/Button/Button';
 import Modal from '@/components/common/Modal/ConfirmModal';
 
 export default function MyPage() {
-  const userData = {
-    name: '김멋사',
-    email: 'likelion@example.com',
-    profileImage: '',
+  const userData = useLoaderData(); // 데이터 가져오기
+
+  const defaultProfileImage = ''; // 기본 이미지
+  const navigate = useNavigate();
+
+  const hasApplication = userData.documentSubmitted; // 지원서 제출 여부
+
+  const canReschedule = userData.interviewScheduleSubmitted && userData.interviewScheduleChangable; // 면접 일정 변경 가능 여부
+
+  const fetcher = useFetcher();
+
+  // 로그아웃
+  const handleLogout = () => {
+    setIsModalOpen(false); // 모달 닫고 로그아웃 해야함
+
+    fetcher.submit(null, { method: 'post', action: '/logout' }); // action 실행하기 위함
   };
 
-  const defaultProfileImage = '';
-
-  // TODO: 지원서 존재여부로 지정
-  const [hasApplication, _setHasApplication] = useState(false);
-  const navigate = useNavigate();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  // TODO: 추후 면접 예약 여부로 지정
-  const [isInterviewReserved, _setIsInterviewReserved] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 관리
+  const [isError, setIsError] = useState(false); // 이미지 로딩 실패
+  const [preview, setPreview] = useState(null); // 이미지 미리보기
 
   const buttonStyle = `
     w-full h-10 pad:h-12 bg-white border border-black
@@ -34,9 +42,6 @@ export default function MyPage() {
 
   const minHeight =
     'min-h-[calc(100vh-52px)] pad:min-h-[calc(100vh-68px)] web:min-h-[calc(100vh-76px)]';
-
-  const [isError, setIsError] = useState(false);
-  const [preview, setPreview] = useState(null);
 
   // 프로필 이미지 변경하는 함수 -> 추후 이미지 백엔드에게 전달 필요!
   const handleFileChange = (e) => {
@@ -58,7 +63,7 @@ export default function MyPage() {
       className={`w-full ${minHeight} relative flex flex-col items-center web:items-stretch justify-center bg-white isolate overflow-hidden"`}
     >
       <div className="flex flex-col web:flex-row web:items-start items-center web:justify-between px-6 pad:px-20 web:px-36 gap-16">
-        {/* 왼쪽 부분 */}
+        {/* 좌측 개인정보 부분 */}
         <div className="relative flex flex-col web:flex-row justify-start items-center gap-x-9 gap-y-14">
           <div className="w-40 h-40 pad:w-44 pad:h-44 relative">
             <div className="relative w-40 h-40 pad:w-44 pad:h-44 bg-zinc-300 border border-black group">
@@ -71,7 +76,7 @@ export default function MyPage() {
                   src={
                     isError
                       ? defaultProfileImage
-                      : preview || userData.profileImage || defaultProfileImage
+                      : preview || userData.profieImageUrl || defaultProfileImage
                   }
                   onError={() => setIsError(true)}
                   className="w-full h-full object-cover group-hover:brightness-75"
@@ -126,7 +131,7 @@ export default function MyPage() {
             </Button>
           </div>
           {/* 면접 예약 여부로 생겼다가 없어져야 하는 면접 일정 수정하기 버튼 -> 추후 어색하면 스켈레톤 넣자 */}
-          {isInterviewReserved && (
+          {canReschedule && (
             <div className="self-stretch">
               <Button
                 onClick={() => {
@@ -166,11 +171,7 @@ export default function MyPage() {
           </div>
         </div>
       </div>
-      <Modal
-        isOpen={isModalOpen}
-        cancel={() => setIsModalOpen(false)}
-        confirm={() => setIsModalOpen(false) /* TODO: 추후 이동할 페이지 추가 필요 */}
-      >
+      <Modal isOpen={isModalOpen} cancel={() => setIsModalOpen(false)} confirm={handleLogout}>
         로그아웃 하시겠습니까?
       </Modal>
       <div className="w-72 h-72 pad:w-113.5 pad:h-96 web:w-146 web:h-145 left-18.75 bottom-0 pad:left-69.5 pad:bottom-0 web:left-186 web:bottom-0 absolute -z-10">
