2d43dcfcc8d934fa1f0e219e4d489638354bdb95
hyeonsu_ <117705439+agness4011@users.noreply.github.com>
2026-02-05 19:59:49 +0900

    🎨 Design: 회원가입 완료 페이지(welcome) 디자인 수정

--- src/pages/Login/Welcome.jsx
+++ src/pages/Login/Welcome.jsx
@@ -1,5 +1,10 @@
+import WelcomeBackground from '@/components/login/WelcomeBackground';
 import WelcomeSection from '@/components/login/WelcomeSection';
 
 export default function WelcomePage() {
-  return <WelcomeSection />;
+  return (
+    <WelcomeBackground>
+      <WelcomeSection />
+    </WelcomeBackground>
+  );
 }
