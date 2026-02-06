da25c4abb31eaa61a65d1fbcdb4e21dd02e1cd1e
simGPT <simGPT02@gmail.com>
2026-02-06 10:33:30 +0900

    :sparkles:Feat: 트랙별 질문 조회 및 임시저장 지원서 트랙별 답변 조회 api 로더 추가

--- src/api/applyLoader.js
+++ src/api/applyLoader.js
@@ -1,11 +1,20 @@
 import { APIService } from '@/api/api';
 
-export const basicInfoLoader = async () => {
+export const applyLoader = async () => {
   try {
-    const response = await APIService.private.get('/v1/applications/records/personal-info');
-    return response.data || {}; // 응답 없어도 기본값 보장
+    const [basicInfoRes, QuestionRes, recordAnswerRes] = await Promise.all([
+      APIService.private.get('/v1/applications/records/personal-info'), // 기본 인적사항 조회 api
+      APIService.private.get('/v1/applications/questions'), // 트랙별 질문 조회 api
+      APIService.private.get('/v1/applications/records/draft/answers'), // 임시저장 지원서 트랙별 답변 조회 api
+    ]);
+
+    return {
+      basicInfo: basicInfoRes.data || {},
+      Question: QuestionRes.data || {},
+      recordAnswer: recordAnswerRes.data || {},
+    };
   } catch (error) {
-    console.log('기본 인적사항 정보 조회 실패:', error);
-    throw error; // 에러 두번 던짐
+    console.error('데이터 통합 로딩 실패:', error);
+    throw error;
   }
 };
