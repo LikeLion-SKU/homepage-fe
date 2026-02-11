39b78300d787f739d45aeda33b1f55d083943c85
Jeong mokjin <jungmokjin@gmail.com>
2026-02-11 22:22:10 +0900

    ♻️ Refactor: 지원결과 날짜 계산식 수정

--- src/utils/showResultButton.js
+++ src/utils/showResultButton.js
@@ -1,16 +1,26 @@
+import dayjs from 'dayjs';
+
 import { getCurrentForm } from '@/api/applicationForm';
 
 export const showResultButton = async () => {
-  const nowDate = new Date().getTime();
   const resultDate = await getCurrentForm();
+
   if (!resultDate || !resultDate.applicationResultAt || !resultDate.finalResultAt) {
     console.warn('진행 중인 지원 일정 데이터가 없습니다.');
     return false;
   }
-  const applicationResultAt = new Date(resultDate?.applicationResultAt).getTime();
-  const finalResultAt = new Date(resultDate?.finalResultAt);
-  const finalResultEnd = new Date(finalResultAt);
-  finalResultEnd.setDate(finalResultAt.getDate() + 7);
 
-  return applicationResultAt <= nowDate && nowDate <= finalResultEnd.getTime();
+  const now = dayjs(); // 현재 시간
+
+  // 1. 결과 발표일의 '날짜' 기준 00:00:00
+  const startShow = dayjs(resultDate.applicationResultAt).startOf('day');
+
+  // 2. 최종 발표일의 '날짜' 기준 + 7일 뒤의 23:59:59 (그날 전체 포함)
+  const endShow = dayjs(resultDate.finalResultAt).add(7, 'day').endOf('day');
+
+  // 현재 시간이 시작점보다 같거나 늦고, 끝점보다 같거나 이르면 true
+  return (
+    (now.isAfter(startShow) || now.isSame(startShow)) &&
+    (now.isBefore(endShow) || now.isSame(endShow))
+  );
 };
