3759ed3a727b69b2a90f2b5a1a26f77c3d1d6507
Jeong mokjin <jungmokjin@gmail.com>
2026-02-13 19:42:55 +0900

    🎨 Design: 구성원 사진 비율 유지 코드 추가

--- src/components/Member/MemberCard.jsx
+++ src/components/Member/MemberCard.jsx
@@ -5,7 +5,7 @@ export default function MemberCard({ data }) {
     <div className="flex flex-col w-40 h-54 pad:w-54 pad:h-80 web:w-59 border-2">
       <img
         src={data.profileImageUrl || defaultImg}
-        className="w-40 h-37 pad:w-54 pad:h-54 web:w-59 bg-[#F9F9F9]"
+        className="w-40 h-37 pad:w-54 pad:h-54 web:w-59 bg-[#F9F9F9] object-fit-contain object-center"
       />
       <div className="flex flex-col flex-1 px-3 pt-2 pad:px-6 pad:pt-4.5 gap-1 pad:gap-2 bg-[#F8FBE7] border-t-2">
         <p className="text-[1.1rem] pad:text-[1.325rem] font-bold">{data.name}</p>
