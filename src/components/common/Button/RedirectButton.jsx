09b1e2da90fed7f49c23022b9775a013d3e8c1d4
simGPT <simGPT02@gmail.com>
2026-02-15 11:43:13 +0900

    :recycle:Refactor: 버튼 클릭 영역 확장

--- src/components/common/Button/RedirectButton.jsx
+++ src/components/common/Button/RedirectButton.jsx
@@ -3,16 +3,15 @@ import Button from '@/components/common/Button/Button';
 
 export default function RedirectButton({ buttonName, onClick }) {
   return (
-    <div className="flex justify-center items-center z-10">
-      <Button
-        onClick={onClick}
-        data-variant=""
-        data-size=""
-        className="w-10 h-10 pad:w-12 pad:h-12 bg-button-green hover:bg-button-hover outline flex flex-col justify-center items-center gap-2.5"
-      >
+    <div
+      onClick={onClick}
+      className="flex justify-center items-center z-10 cursor-pointer"
+      role="button"
+    >
+      <div className="w-10 h-10 pad:w-12 pad:h-12 bg-button-green hover:bg-button-hover outline flex flex-col justify-center items-center gap-2.5">
         <img src={Navy} className="w-4 h-4 pad:w-6 pad:h-5" alt="navy icon" />
-      </Button>
-      <div className="self-stretch h-10 pad:h-12 px-4 py-3 outline bg-button-gray relative z-10 flex items-center justify-center">
+      </div>
+      <div className="self-stretch h-10 pad:h-12 px-4 py-3 outline bg-button-gray hover:bg-stone-200 relative z-10 flex items-center justify-center transition-all">
         <div className="justify-center text-zinc-900 text-sm font-semibold pad:text-lg pad:font-medium font-['Pretendard']">
           {buttonName}
         </div>
